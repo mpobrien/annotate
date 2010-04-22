cfc2ebf0d9ac748da531dfac189c1e0986d7f1d4
mike o'brien <mike@meetup.com>
2010-04-22 19:29:45 -0400

    first commit

---
+++ src/annotate/ctrlrs/HomeController.java
@@ -0,0 +1,16 @@
+package annotate.ctrlrs;
+import com.mob.web.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.log4j.*;
+
+@At("^/$")
+public class HomeController extends Controller{
+	Logger log = Logger.getLogger( HomeController.class );
+
+    @Override
+    public WebResponse get(HttpServletRequest req, HttpServletResponse res){
+        return new StringWebResponse("Hello World!");
+    }
+
+}
